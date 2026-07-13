9cf41d52b57e689feb14196cb930838aa702f85e
Dylan Jiang <dylanjiang@proton.me>
2026-07-13 16:41:25 -0600

    moved the printing square section of code to a seperate function

--- square.c
+++ square.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void print_square(int height, int arg_count);
 
 int main(int argc, char *argv[])
 {
-    int height = atoi(argv[1]);
-
-    if (!( argc == 2 ))
+    if (strcmp(argv[1], "square") == 0)
     {
-        printf("Error: maximum amount of arguments is 1");
-        return 1;
+        print_square(atoi(argv[2]) , argc);
+    }
+}
+
+void print_square(int height, int arg_count)
+{
+
+    if (!( arg_count == 3 ))
+    {
+        printf("Error: printing a square requires 2 arguments.\n");
     }
     else
     {
@@ -20,6 +29,5 @@ int main(int argc, char *argv[])
             }
             printf("\n");
         }
-        return 0;
     }
 }
