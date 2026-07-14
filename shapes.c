7b9a00c06069268e4feb0ce9c27bd253d1929a5a
Dylan Jiang <dylanjiang@proton.me>
2026-07-14 16:59:30 -0600

    added function for printing rectangles and added more error chacking

--- shapes.c
+++ shapes.c
@@ -2,17 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-void print_square(int height);
-void print_triangle(char *align, int height);
+void print_square(int length);
+void print_rectangle(int length, int width);
+void print_right_triangle(char *align, int height);
 
 int main(int argc, char *argv[])
 {
-    if (strcmp(argv[1], "-s") == 0 ||strcmp(argv[1], "-square") == 0)
+    if (strcmp(argv[1], "-s") == 0 ||strcmp(argv[1], "--square") == 0)
     {
         if (!( argc == 3 ))
         {
             printf("Error: printing a square requires 2 arguments.\n");
             return 1;
+        }
+        else if (atoi(argv[2]) == 0)
+        {
+            printf("Error: unknown arguments.");
+            return 2;
         }
         else
         {
@@ -24,12 +30,35 @@ int main(int argc, char *argv[])
     {
         if (!( argc == 4 ))
         {
-            printf("Error: printing a triangle requires 3 arguments.\n");
+            printf("Error: printing a right triangle requires 3 arguments.\n");
             return 1;
+        }
+        else if (atoi(argv[3]) == 0)
+        {
+            printf("Error: unknown arguments.");
+            return 2;
         }
         else
         {
-            print_triangle(argv[2], atoi(argv[3]));
+            print_right_triangle(argv[2], atoi(argv[3]));
+            return 0;
+        }
+    }
+    else if (strcmp(argv[1], "-r") == 0 || strcmp(argv[1], "--rectangle") == 0)
+    {
+        if (!(argc == 4 ))
+        {
+            printf("Error: printing a rectangle requires 3 arguments.\n");
+            return 1;
+        }
+        else if (atoi(argv[2]) == 0 || atoi(argv[3]) == 0)
+        {
+            printf("Error: unknown arguments.");
+            return 2;
+        }
+        else
+        {
+            print_rectangle(atoi(argv[2]), atoi(argv[3]));
             return 0;
         }
     }
@@ -40,11 +69,11 @@ int main(int argc, char *argv[])
     }
 }
 
-void print_square(int height)
+void print_square(int length)
 {
-    for (int i = 0; i < height; i++)
+    for (int i = 0; i < length; i++)
     {
-        for (int j = 0; j < height; j++)
+        for (int j = 0; j < length; j++)
         {
             printf("#");
         }
@@ -52,9 +81,21 @@ void print_square(int height)
     }
 }
 
-void print_triangle(char *align, int height)
+void print_rectangle(int length, int width)
 {
-    if (strcmp(align, "-l") == 0 ||strcmp(align, "--length") == 0)
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+void print_right_triangle(char *align, int height)
+{
+    if (strcmp(align, "-l") == 0 ||strcmp(align, "--left") == 0)
     {
         for (int i = 1; i <= height; i++)
         {
